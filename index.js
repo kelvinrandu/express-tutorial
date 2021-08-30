154aab916de1cbedbea50a8880cc3bedf6bf6283
kelvinrandu <randukelvin@gmail.com>
2021-08-30 16:48:19 +0300

    added crud functionality

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const path = require('path');
-const { send } = require('process');
-const users = require('./Users');
+const express = require("express");
+const path = require("path");
 
+const logger = require("./Middleware/logger");
 
 const app = express();
 
-const logger =( req,res,next) =>{
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-};
-app.use(logger);
+// app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.get('/api/users', (req,res) => res.json(users));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/api/users', require('./routes/api/users'))
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
